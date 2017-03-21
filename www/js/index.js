d19c33a3a06173d4e2b103afaf9924255a8ae1ef
NagarjunSaragadam <nagarjuns.2750@kpcl.local>
2017-03-21 11:19:01 +0530

    do

--- www/js/index.js
+++ www/js/index.js
@@ -10,7 +10,7 @@ var app = {
     macAddress: "00:00:00:00:00:01",  // get your mac address from bluetoothSerial.list
     chars: "",
     trackGpsDelay: 400,
-    carWatchDelay: 300,
+    carWatchDelay: 1000,
     deepMode: false,
     trackServerDeepDelay: 60000 * 10,
     trackServerDelay: 500,
