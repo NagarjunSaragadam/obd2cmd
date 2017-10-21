142ffff1af78ab92a85b10ef44630f0a1cbf8405
Nagarjun S <thenagarjun558@gmail.com>
2017-10-21 14:40:42 -0700

    Commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,36 +1,17 @@
-
 var app = {
-   
-    Application constructor
- */
-    initialize: function() {
-        this.bindEvents();		
-	},
+        // Application Constructor
+        initialize: function () {
+            app.bindEvents();
+        },
+        bindEvents: function () {
+            document.addEventListener('deviceready', app.onDeviceReady, false);
+        },
+        onDeviceReady: function () {
+            window.plugins.imeiplugin.getImei(callback);    
+        }
+    };
+    function callback(imei) {
+        var element=document.getElementById("demo");
+        element.value=imei;
 
-/*
-    bind any events that are required on startup to listeners:
-*/
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);  		
-    },
-
-/*
-    this runs when the device is ready for user interaction:
-*/
-    onDeviceReady: function() {  	    
-       app.startTrackCar();
-    },
-	
-    startTrackCar: function(){
-     window.plugins.imei.get(
-  function(imei) {
-    alert("got imei: " + imei);
-  },
-  function() {
-    alert("error loading imei");
-  }
-);
     }
-	
-};      // end of app
-
