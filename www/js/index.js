6ee1e319d6fdd3e370c9851979c20eb6f1c23555
Nagarjun S <thenagarjun558@gmail.com>
2017-10-23 15:55:24 +0530

    update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@
 var app = {
    
     
- */
+ 
     initialize: function() {
         this.bindEvents();		
 	},
@@ -18,16 +18,14 @@ var app = {
     this runs when the device is ready for user interaction:
 */
     onDeviceReady: function() {  	    
-       var deviceInfo = cordova.require("cordova/plugin/DeviceInformation");
-deviceInfo.get(function(result) {
-        alert("result = " + result);
-    }, function() {
-        alert("error");
-    });
+alert(device.uuid);
     }
 	
    
+
+
+
     
 	
-};      // end of app
+};     
 
