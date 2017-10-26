84df8a1b4d2e0a9340a7af93bc17dbddbe00d390
NagarjunSaragadam <thenagarjun558@gmail.com>
2017-10-26 14:59:49 +0530

    Commit

--- www/js/index.js
+++ www/js/index.js
@@ -21,6 +21,25 @@ var app = {
 		var sdsd=device.uuid;
         alert(device.uuid);	 
 		document.getElementById('deviceid').innerHTML=sdsd;
+		
+		
+		$("#scan").click(function () {
+            app.scan();
+        });
+    },
+	
+	 scan: function() {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                if (!result.cancelled) {
+                    alert(result.text+document.getElementById('deviceid').innerHTML);													
+                    window.location.href = 'http://nagarjun558.5gbfree.com/index2.html?Fileid='+result.text+'$'+document.getElementById('deviceid').innerHTML;
+                }
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+       );
     }
 	
    
