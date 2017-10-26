6ba327125d28417d9a9bbc6346d1de7d728476f0
NagarjunSaragadam <thenagarjun558@gmail.com>
2017-10-26 16:08:49 +0530

    Final

--- www/js/index.js
+++ www/js/index.js
@@ -18,8 +18,7 @@ var app = {
     this runs when the device is ready for user interaction:
 */
     onDeviceReady: function() {  	
-		var sdsd=device.uuid;
-        alert(device.uuid);	 
+		var sdsd=device.uuid;        
 		document.getElementById('deviceid').innerHTML=sdsd;
 		$("#scan").click(function () {
             app.scan();
@@ -32,10 +31,8 @@ var app = {
                 if (!result.cancelled) {
 					var str = document.getElementById('deviceid').innerHTML;
                     str = str.replace(/[^0-9]+/ig,"");					
-					var count=((parseInt(result.text)+parseInt(str))*20)/34;
-                    alert(result.text+document.getElementById('deviceid').innerHTML);		
-					document.getElementById('Keyresult').innerHTML='Your File Key'+count;
-                    /*window.location.href = 'http://nagarjun558.5gbfree.com/index2.html?Fileid='+result.text+'$'+document.getElementById('deviceid').innerHTML;*/
+					var count=((parseInt(result.text)+parseInt(str))*20)/34;                    
+					document.getElementById('Keyresult').innerHTML='File ID:'+result.text+'<br/> Key:'+count;                    
                 }
             },
             function (error) {
